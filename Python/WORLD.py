7a7d5a9ea41ffe569c30e7fde6bf67a0001d3a34
Dannn <Jordaks>
2024-06-26 16:26:17 +0800

    knowledge

--- Python/WORLD.py
+++ Python/WORLD.py
@@ -1,3 +1,19 @@
-name = "Jordann"
+name = "Mark Jordan Javier"
+txt = "I'm learning python"
+text = "I love pizza!"
 
-print(name[3:-1])
+
+print("My name is " + name)
+print(txt)
+
+length = len(txt)
+
+print("The length of my text is " + str(length))
+print("The length of my name is " + str(len(name)))
+
+new_text = text[:7] + "Burger"
+
+new_txt = text[:0] + "My favorite is Spaghetti"
+
+print(new_text)
+print(new_txt)
