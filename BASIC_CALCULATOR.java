a34503fd67b52fab4b1e9ac7c1dbb8a8e481090b
Dannn <Jordaks>
2024-09-03 13:20:51 +0800

    done basic calculator

--- BASIC_CALCULATOR.java
+++ BASIC_CALCULATOR.java
@@ -3,34 +3,39 @@ import java.util.Scanner;
 
 public class BASIC_CALCULATOR {
     public static void main(String[]args){
-        do {
 
+        String operation;
+        do {
         System.out.println("\t\nBASIC CALCULATOR\n");
 
         System.out.println("Enter a first number: ");
 
         Scanner num1 = new Scanner(System.in);
-        int Num1 = num1.nextInt();
+        double Num1 = num1.nextInt();
 
         System.out.println("Enter a second number: ");
 
         Scanner num2 = new Scanner(System.in);
-        int Num2 = num2.nextInt();
+        double Num2 = num2.nextInt();
 
-        System.out.println("Enter Operation ( + , - , * , / ): ");
+        System.out.println("Enter Operation ( + , - , * , / , %): ");
         
         Scanner operator = new Scanner(System.in);
         String operators = operator.nextLine();
 
         
         if(operators.equals("+")){
-            System.out.println("\nAnswer is " + (Num1 + Num2));
+            System.out.println("\nAnswer is " + (int)(Num1 + Num2));
         }else if(operators.equals("-")){
-            System.out.println("\nAnswer is " + (Num1 - Num2));
+            System.out.println("\nAnswer is " + (int)(Num1 - Num2));
         }else if(operators.equals("*")){
-            System.out.println("\nAnswer is " + (Num1 * Num2));
+            System.out.println("\nAnswer is " + (int)(Num1 * Num2));
         }else if(operators.equals("/")){
-            System.out.println("\nAnswer is " + (Num1 / Num2));
+            if (Num2 == 0) {
+                System.out.println("\nUndefined!");
+            }else{
+                System.out.println("\nAnswer is " + (Num1 / Num2));
+            }
         }else{
             System.out.println("\nInvalid Input!!");
         }
@@ -38,9 +43,9 @@ public class BASIC_CALCULATOR {
         System.out.println("\nDo you want to perform another operation? (y/n): ");
 
         Scanner loop = new Scanner(System.in);
-        String operation = loop.nextLine();
+        operation = loop.nextLine();
 
-        } while(true||false);
+        } while(operation.equals("y")||operation.equals("Y"));
         
     }
 }
