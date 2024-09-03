e86ca6477ebdf2ed6bfe4ac8283b75db221ff51d
Dannn <Jordaks>
2024-09-03 12:01:54 +0800

    new

--- BASIC_CALCULATOR.java
+++ BASIC_CALCULATOR.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 
 public class BASIC_CALCULATOR {
     public static void main(String[]args){
+        do {
+
         System.out.println("\t\nBASIC CALCULATOR\n");
 
         System.out.println("Enter a first number: ");
@@ -20,25 +22,25 @@ public class BASIC_CALCULATOR {
         Scanner operator = new Scanner(System.in);
         String operators = operator.nextLine();
 
-        do {
+        
         if(operators.equals("+")){
-            System.out.println("Answer is " + (Num1 + Num2));
+            System.out.println("\nAnswer is " + (Num1 + Num2));
         }else if(operators.equals("-")){
-            System.out.println("Answer is " + (Num1 - Num2));
+            System.out.println("\nAnswer is " + (Num1 - Num2));
         }else if(operators.equals("*")){
-            System.out.println("Answer is " + (Num1 * Num2));
+            System.out.println("\nAnswer is " + (Num1 * Num2));
         }else if(operators.equals("/")){
-            System.out.println("Answer is " + (Num1 / Num2));
+            System.out.println("\nAnswer is " + (Num1 / Num2));
         }else{
-            System.out.println("Invalid Input!!");
+            System.out.println("\nInvalid Input!!");
         }
 
         System.out.println("\nDo you want to perform another operation? (y/n): ");
 
         Scanner loop = new Scanner(System.in);
-        String yorn = loop.nextLine();
+        String operation = loop.nextLine();
 
-        } while(yorn == true||yorn == true);
+        } while(true||false);
         
     }
 }
