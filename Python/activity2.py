f03998542d3bab6e767a79e54ea87f7124ac3d95
Dannn <Jordaks>
2024-09-26 22:37:17 +0800

    new

--- Python/activity2.py
+++ Python/activity2.py
@@ -6,7 +6,7 @@ time = int(input("Enter the time of the show (in 24-hour format, e.g., 14 for 2 
 if age < 12:
     if time <= 17:
         print("Hello " + name + "!" , "The ticket price for you is: $5. Thank you!")
-    elif(time <= 24):
+    elif time <= 24:
         print("Hello " + name + "!" , "The ticket price for you is: $7. Thank you!")
     else:
         print("Invalid Time!!")
@@ -14,14 +14,14 @@ if age < 12:
 elif age <= 60:
     if time <= 17:
         print("Hello " + name + "!" , "The ticket price for you is: $10. Thank you!")
-    elif(time <= 24):
+    elif time <= 24:
         print("Hello " + name + "!" , "The ticket price for you is: $12. Thank you!")
     else:
         print("Invalid Time!!")
 else:
     if time <= 17:
         print("Hello " + name + "!" , "The ticket price for you is: $6. Thank you!")
-    elif(time <= 24):
+    elif time <= 24:
         print("Hello " + name + "!" , "The ticket price for you is: $8. Thank you!")
     else:
         print("Invalid Time!!")
