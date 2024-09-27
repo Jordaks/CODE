c48772719b410a9f705681f24904c4bb12534d67
Dannn <Jordaks>
2024-09-27 23:39:06 +0800

    done act

--- Calculator.java
+++ Calculator.java
@@ -1,31 +1,34 @@
-
 import java.util.Scanner;
 
-public class activity {
+public class Calculator {
+        class MathUtils{
+            static int multiply(int a, int b){
+                return a * b;
+            }
+        }
     public static int myInt(int a, int b){
         return  a + b;
     }
     public static double myDouble(double a, double b){
         return a + b;
     }
-    public static int MyMultiply(int a, int b){
-        return a * b;
-    }
     public static void main(String[] args) {
         int a, b;
-        System.out.print("Enter a first number: ");
-        Scanner first_Num = new Scanner(System.in);
-        a = first_Num.nextInt();
-        System.out.print("Enter a second number: ");
-        b = first_Num.nextInt();
+        Scanner Num = new Scanner(System.in);
 
+        System.out.print("Enter a first number: ");
+        a = Num.nextInt();
+        System.out.print("Enter a second number: ");
+        b = Num.nextInt();
 
         int sum = myInt(a,b);
-        double Double_sum = myDouble(a, b);
-        int multiply = MyMultiply(a, b);
+        double Double_sum = myDouble(a,b);
+        Calculator calculator = new Calculator();
+        Calculator.MathUtils Multi = calculator.new MathUtils();
 
         System.out.println(a + " + " + b + " = " + sum);
         System.out.println(a + " + " + b + " = " + Double_sum);
-        System.out.println(a + " * "  + b + " = " + multiply);
+        System.out.println(a + " * " + b + " = " + Multi.multiply(a, b));
+        
     }
 }
